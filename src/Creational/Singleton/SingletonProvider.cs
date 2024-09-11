65eec36082cad9a180139fd9d33a2ad7872d99e6
fatihd <fatihdikdere@gmail.com>
2024-09-12 00:06:53 +0300

    Singleton design pattern was added

---
+++ src/Creational/Singleton/SingletonProvider.cs
@@ -0,0 +1,25 @@
+﻿namespace Singleton;
+public static class SingletonProvider<TInstance> where TInstance : class, new()
+{
+    private static  TInstance? instance;
+    private static readonly object @lock = new();
+    public static TInstance Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                lock (@lock)
+                {
+                    instance ??= new TInstance();
+                }
+            }
+
+            return instance;
+        }
+    }
+    static SingletonProvider()
+    {
+
+    }
+}
