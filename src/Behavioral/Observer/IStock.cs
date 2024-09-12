da283bba83d4841dd5ffd75a4150aeaa7c7a2b4c
fatihd <fatihdikdere@gmail.com>
2024-09-12 20:07:28 +0300

    Observer design pattern was added

---
+++ src/Behavioral/Observer/IStock.cs
@@ -0,0 +1,11 @@
+﻿namespace Observer;
+/// <summary>
+/// Subject interface
+/// </summary>
+public interface IStock
+{
+    decimal Price { get; set; }
+    void Attach(IInvestor investor);
+    void Detach(IInvestor investor);
+    void Notify();
+}
---
+++ src/Behavioral/Observer/Investor.cs
@@ -0,0 +1,13 @@
+﻿namespace Observer;
+/// <summary>
+/// ConcreteObserver class
+/// </summary>
+/// <seealso cref="Observer.IInvestor" />
+public class Investor(string name) : IInvestor
+{
+    private readonly string name = name;
+    public void Update(IStock stock)
+    {
+        Console.WriteLine($"Investor {name} notified of price change: {stock?.Price}");
+    }
+}
