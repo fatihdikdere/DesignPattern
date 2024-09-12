2aa97070230e99e815d32e75b22a61c3b1685922
fatihd <fatihdikdere@gmail.com>
2024-09-12 20:12:40 +0300

    TemplateMethod.Test was added

---
+++ test/Behavioral/TemplateMethod.Test/CaffeineBeverageTests.cs
@@ -0,0 +1,20 @@
+﻿namespace TemplateMethod.Test;
+public class CaffeineBeverageTests
+{
+    [Fact]
+    public void PrepareRecipe_WhenCalled_ShouldCallMethodsInCorrectOrder()
+    {
+        // Arrange
+        var beverage = new TestCaffeineBeverage();
+
+        // Act
+        beverage.PrepareRecipe();
+        beverage.VerifySequence();
+
+        // Assert
+        beverage.BoilWaterCalled.Should().BeTrue();
+        beverage.PourInCupCalled.Should().BeTrue();
+        beverage.BrewOrSteepCalled.Should().BeTrue();
+        beverage.AddCondimentsCalled.Should().BeTrue();
+    }
+}
