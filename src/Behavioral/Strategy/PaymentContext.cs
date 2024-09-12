5919c6e5ac8d0422aa9665e3d274984d89f0aba7
fatihd <fatihdikdere@gmail.com>
2024-09-12 20:10:37 +0300

    Strategy design pattern was added

---
+++ src/Behavioral/Strategy/PaymentContext.cs
@@ -0,0 +1,12 @@
+﻿namespace Strategy;
+/// <summary>
+/// Context class
+/// </summary>
+public class PaymentContext(IPaymentStrategy paymentStrategy)
+{
+    private readonly IPaymentStrategy paymentStrategy = paymentStrategy;
+    public bool ExecutePayment(decimal amount)
+    {
+        return paymentStrategy.ProcessPayment(amount);
+    }
+}
---
+++ src/Behavioral/Strategy/Program.cs
@@ -0,0 +1,13 @@
+﻿using Strategy;
+
+PaymentContext paymentContext;
+
+var creditCardPayment = new CreditCardPayment();
+paymentContext = new PaymentContext(creditCardPayment);
+paymentContext.ExecutePayment(100);
+
+var bitcoinPayment = new BitcoinPayment();
+paymentContext = new PaymentContext(bitcoinPayment);
+paymentContext.ExecutePayment(100);
+
+Console.ReadKey();
