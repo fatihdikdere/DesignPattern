6ef96eed4f060ca3f474a5d62061df2a1b6b29df
fatihd <fatihdikdere@gmail.com>
2024-09-12 20:14:46 +0300

    Memento design pattern was added

---
+++ src/Behavioral/Memento/GameSaveManager.cs
@@ -0,0 +1,23 @@
+﻿namespace Memento;
+/// <summary>
+/// Caretaker class
+/// </summary>
+public class GameSaveManager
+{
+    private readonly Stack<GameSaveMemento> saveHistory = new();
+    public void SaveGame(Game game)
+    {
+        saveHistory.Push(game.Save());
+        Console.WriteLine("Game saved");
+    }
+    public void LoadLastSave(Game game)
+    {
+        if (saveHistory.Count <= 0)
+        {
+            return;
+        }
+
+        game.Load(saveHistory.Pop());
+        Console.WriteLine("Game loaded");
+    }
+}
---
+++ src/Behavioral/Memento/GameSaveMemento.cs
@@ -0,0 +1,10 @@
+﻿namespace Memento;
+/// <summary>
+/// Memento class
+/// </summary>
+public class GameSaveMemento(int health, int position, List<string> inventory)
+{
+    public int Health { get; } = health;
+    public int Position { get; } = position;
+    public List<string> Inventory { get; } = inventory;
+}
