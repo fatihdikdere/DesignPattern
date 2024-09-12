09a25bc2485147ef4f2151d87f67e7be7f55fbba
fatihd <fatihdikdere@gmail.com>
2024-09-12 20:55:58 +0300

    Visitor.Test was added

---
+++ test/Behavioral/Visitor.Test/DeveloperTests.cs
@@ -0,0 +1,17 @@
+﻿namespace Visitor.Test;
+public class DeveloperTests
+{
+    [Fact]
+    public void Accept_WhenCalled_ShouldCallVisitDeveloper()
+    {
+        // Arrange
+        var developer = new Developer();
+        var visitorMock = new Mock<IReviewVisitor>();
+
+        // Act
+        developer.Accept(visitorMock.Object);
+
+        // Assert
+        visitorMock.Verify(v => v.Visit(developer), Times.Once);
+    }
+}
