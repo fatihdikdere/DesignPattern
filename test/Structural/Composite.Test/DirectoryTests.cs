e48dcb55d0afe9a643325db2807a1dbaa16121b4
fatihd <fatihdikdere@gmail.com>
2024-09-13 23:00:33 +0300

    Composite.Test was added

---
+++ test/Structural/Composite.Test/DirectoryTests.cs
@@ -0,0 +1,81 @@
+﻿namespace Composite.Test;
+public class DirectoryTests
+{
+    [Fact]
+    public void AddItem_WhenCalled_ShouldAddItemToDirectory()
+    {
+        // Arrange
+        var directory = new Directory("TestDirectory");
+        var itemMock = new Mock<IFileSystemItem>();
+
+        // Act
+        directory.AddItem(itemMock.Object);
+
+        // Assert
+        directory.Items.Should().Contain(itemMock.Object);
+    }
+
+    [Fact]
+    public void RemoveItem_WhenCalled_ShouldRemoveItemFromDirectory()
+    {
+        // Arrange
+        var directory = new Directory("TestDirectory");
+        var itemMock = new Mock<IFileSystemItem>();
+
+        directory.AddItem(itemMock.Object);
+
+        // Act
+        directory.RemoveItem(itemMock.Object);
+
+        // Assert
+        directory.Items.Should().NotContain(itemMock.Object);
+    }
+
+    [Fact]
+    public void GetSize_WhenCalled_ShouldReturnTotalSizeOfAllItems()
+    {
+        // Arrange
+        var directory = new Directory("TestDirectory");
+        var mockFile1 = new Mock<IFileSystemItem>();
+        var mockFile2 = new Mock<IFileSystemItem>();
+
+        mockFile1.Setup(f => f.GetSize()).Returns(100);
+        mockFile2.Setup(f => f.GetSize()).Returns(200);
+
+        directory.AddItem(mockFile1.Object);
+        directory.AddItem(mockFile2.Object);
+
+        // Act
+        var totalSize = directory.GetSize();
+
+        // Assert
+        totalSize.Should().Be(300);
+    }
+
+    [Fact]
+    public void ToString_WhenCalled_ShouldReturnCorrectStringRepresentation()
+    {
+        // Arrange
+        var directory = new Directory("TestDirectory");
+        var mockFile1 = new Mock<IFileSystemItem>();
+        var mockFile2 = new Mock<IFileSystemItem>();
+
+        mockFile1.Setup(f => f.ToString()).Returns("File1.txt => Size: 100");
+        mockFile2.Setup(f => f.ToString()).Returns("File2.txt => Size: 200");
+
+        directory.AddItem(mockFile1.Object);
+        directory.AddItem(mockFile2.Object);
+
+        var expected =
+            "Directory: TestDirectory" + Environment.NewLine +
+            Environment.NewLine +
+            "File1.txt => Size: 100" + Environment.NewLine +
+            "File2.txt => Size: 200" + Environment.NewLine;
+
+        // Act
+        var result = directory.ToString();
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
---
+++ test/Structural/Composite.Test/FileTests.cs
@@ -0,0 +1,29 @@
+﻿namespace Composite.Test;
+public class FileTests
+{
+    [Fact]
+    public void GetSize_WhenCalled_ShouldReturnCorrectSize()
+    {
+        // Arrange
+        var file = new File("TestFile.txt", 1024);
+
+        // Act
+        var size = file.GetSize();
+
+        // Assert
+        size.Should().Be(1024);
+    }
+
+    [Fact]
+    public void ToString_WhenCalled_ShouldReturnCorrectStringRepresentation()
+    {
+        // Arrange
+        var file = new File("TestFile.txt", 1024);
+
+        // Act
+        var result = file.ToString();
+
+        // Assert
+        result.Should().Be("File: TestFile.txt => Size: 1024");
+    }
+}
