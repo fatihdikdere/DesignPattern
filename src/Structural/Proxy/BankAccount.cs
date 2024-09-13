fb3d28024b64979f43dfb5c16a2b2998322a5bee
fatihd <fatihdikdere@gmail.com>
2024-09-13 22:20:54 +0300

    Proxy design pattern was added

---
+++ src/Structural/Proxy/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿namespace Proxy;
+/// <summary>
+/// RealSubject class
+/// </summary>
+/// <seealso cref="Proxy.IBankAccount" />
+public class BankAccount(double balance) : IBankAccount
+{
+    private double balance = balance;
+    public double Balance => balance;
+    public bool Withdraw(double amount)
+    {
+        if (amount > balance)
+        {
+            Console.WriteLine("Insufficient balance");
+
+            return false;
+        }
+
+        balance -= amount;
+
+        Console.WriteLine($"New balance {balance}");
+
+        return true;
+    }
+}
---
+++ src/Structural/Proxy/BankAccountProxy.cs
@@ -0,0 +1,14 @@
+﻿namespace Proxy;
+/// <summary>
+/// ProxyObject class
+/// </summary>
+/// <seealso cref="Proxy.IBankAccount" />
+public class BankAccountProxy(BankAccount bankAccount) : IBankAccount
+{
+    private readonly BankAccount bankAccount = bankAccount;
+    public double Balance => bankAccount.Balance;
+    public bool Withdraw(double amount)
+    {
+        return bankAccount.Withdraw(amount);
+    }
+}
