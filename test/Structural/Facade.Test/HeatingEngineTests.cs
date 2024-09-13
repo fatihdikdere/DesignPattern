5fc923af0ab7102c2693ec3b73c22e9a1eeb844e
fatihd <fatihdikdere@gmail.com>
2024-09-13 22:23:39 +0300

    Facade.Test was added

---
+++ test/Structural/Facade.Test/HeatingEngineTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Facade.Test;
+public class HeatingEngineTests
+{
+    [Fact]
+    public void IsHeatAverage_WhenMethodCalled_ShouldReturnTrue()
+    {
+        // Arrange
+        var heatingEngine = new HeatingEngine();
+
+        // Act
+        var result = heatingEngine.IsHeatAverage();
+
+        // Assert
+        result.Should().BeTrue();
+    }
+}
---
+++ test/Structural/Facade.Test/HomeAutomationSystemTests.cs
@@ -0,0 +1,103 @@
+﻿namespace Facade.Test;
+public class HomeAutomationSystemTests
+{
+    [Fact]
+    public void IsHomeAbleToBeBuilt_WhenAllEnginesPass_ShouldReturnTrue()
+    {
+        // Arrange
+        var graphicEngineMock = new Mock<GraphicEngine>();
+        var heatingEngineMock = new Mock<HeatingEngine>();
+        var securityEngineMock = new Mock<SecurityEngine>();
+
+        graphicEngineMock.Setup(g => g.IsGraphicEnough()).Returns(true);
+        heatingEngineMock.Setup(h => h.IsHeatAverage()).Returns(true);
+        securityEngineMock.Setup(s => s.IsSystemHasVulnerability()).Returns(false);
+
+        var homeAutomationSystem = new HomeAutomationSystem(
+            graphicEngineMock.Object,
+            heatingEngineMock.Object,
+            securityEngineMock.Object
+        );
+
+        // Act
+        var result = homeAutomationSystem.IsHomeAbleToBeBuilt();
+
+        // Assert
+        result.Should().BeTrue("Home is able to be built");
+    }
+
+    [Fact]
+    public void IsHomeAbleToBeBuilt_WhenGraphicEngineFails_ShouldReturnFalse()
+    {
+        // Arrange
+        var graphicEngineMock = new Mock<GraphicEngine>();
+        var heatingEngineMock = new Mock<HeatingEngine>();
+        var securityEngineMock = new Mock<SecurityEngine>();
+
+        graphicEngineMock.Setup(g => g.IsGraphicEnough()).Returns(false);
+        heatingEngineMock.Setup(h => h.IsHeatAverage()).Returns(true);
+        securityEngineMock.Setup(s => s.IsSystemHasVulnerability()).Returns(false);
+
+        var homeAutomationSystem = new HomeAutomationSystem(
+            graphicEngineMock.Object,
+            heatingEngineMock.Object,
+            securityEngineMock.Object
+        );
+
+        // Act
+        var result = homeAutomationSystem.IsHomeAbleToBeBuilt();
+
+        // Assert
+        result.Should().BeFalse("GraphicEngine error");
+    }
+
+    [Fact]
+    public void IsHomeAbleToBeBuilt_WhemHeatingEngineFails_ShouldReturnFalse()
+    {
+        // Arrange
+        var graphicEngineMock = new Mock<GraphicEngine>();
+        var heatingEngineMock = new Mock<HeatingEngine>();
+        var securityEngineMock = new Mock<SecurityEngine>();
+
+        graphicEngineMock.Setup(g => g.IsGraphicEnough()).Returns(true);
+        heatingEngineMock.Setup(h => h.IsHeatAverage()).Returns(false);
+        securityEngineMock.Setup(s => s.IsSystemHasVulnerability()).Returns(false);
+
+        var homeAutomationSystem = new HomeAutomationSystem(
+            graphicEngineMock.Object,
+            heatingEngineMock.Object,
+            securityEngineMock.Object
+        );
+
+        // Act
+        var result = homeAutomationSystem.IsHomeAbleToBeBuilt();
+
+        // Assert
+        result.Should().BeFalse("HeatingEngine error");
+    }
+
+    [Fact]
+    public void IsHomeAbleToBeBuilt_WhenSecurityEngineFails_ShouldReturnFalse()
+    {
+        // Arrange
+        var graphicEngineMock = new Mock<GraphicEngine>();
+        var heatingEngineMock = new Mock<HeatingEngine>();
+        var securityEngineMock = new Mock<SecurityEngine>();
+
+        graphicEngineMock.Setup(g => g.IsGraphicEnough()).Returns(true);
+        heatingEngineMock.Setup(h => h.IsHeatAverage()).Returns(true);
+        securityEngineMock.Setup(s => s.IsSystemHasVulnerability()).Returns(true);
+
+        var homeAutomationSystem = new HomeAutomationSystem(
+            graphicEngineMock.Object,
+            heatingEngineMock.Object,
+            securityEngineMock.Object
+        );
+
+        // Act
+        var result = homeAutomationSystem.IsHomeAbleToBeBuilt();
+
+        // Assert
+        result.Should().BeFalse("SecurityEngine error");
+    }
+}
