984cbd8f3d1f06e34cd41fe91f6f173082f27e45
fatihd <fatihdikdere@gmail.com>
2024-09-14 16:56:27 +0300

    Decorator.Test was added

---
+++ test/Structural/Decorator.Test/SmsNotifierTests.cs
@@ -0,0 +1,17 @@
+﻿namespace Decorator.Test;
+public class SmsNotifierTests
+{
+    [Fact]
+    public void Send_WhenCalled_ShouldSendBaseNotificationAndSmsNotification()
+    {
+        // Arrange
+        var mockNotifier = new Mock<INotifier>();
+        var smsNotifier = new SmsNotifier(mockNotifier.Object);
+
+        // Act
+        smsNotifier.Send("Test message");
+
+        // Assert
+        mockNotifier.Verify(n => n.Send("Test message"), Times.Once); 
+    }
+}
