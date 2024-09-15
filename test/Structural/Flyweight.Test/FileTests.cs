dbb63515209d1a978bf5c2c4ba25b878a094a2b8
fatihd <fatihdikdere@gmail.com>
2024-09-15 14:50:38 +0300

    Flyweight.Test was added

---
+++ test/Structural/Flyweight.Test/FileTests.cs
@@ -0,0 +1,17 @@
+﻿namespace Flyweight.Test;
+public class FileTests
+{
+    [Fact]
+    public void DisplayIcon_WhenCalled_ShouldCallDisplayMethodOnIconWithFileName()
+    {
+        // Arrange
+        var mockIcon = new Mock<IIcon>();
+        var file = new File("document.txt", mockIcon.Object);
+
+        // Act
+        file.DisplayIcon();
+
+        // Assert
+        mockIcon.Verify(icon => icon.Display("document.txt"), Times.Once);
+    }
+}
---
+++ test/Structural/Flyweight.Test/IconFactoryTests.cs
@@ -0,0 +1,48 @@
+﻿namespace Flyweight.Test;
+public class IconFactoryTests
+{
+    [Fact]
+    public void GetIcon_WhenIconTypeDoesNotExist_ShouldCreateAndReturnNewIcon()
+    {
+        // Arrange
+        var iconFactory = new IconFactory();
+
+        // Act
+        var icon = iconFactory.GetIcon("Text File");
+
+        // Assert
+        icon.Should().NotBeNull();
+        icon.Should().BeOfType<Icon>();
+    }
+
+    [Fact]
+    public void GetIcon_WhenIconTypeAlreadyExists_ShouldReturnExistingIcon()
+    {
+        // Arrange
+        var iconFactory = new IconFactory();
+        var firstIcon = iconFactory.GetIcon("Text File");
+
+        // Act
+        var secondIcon = iconFactory.GetIcon("Text File");
+
+        // Assert
+        secondIcon.Should().BeSameAs(firstIcon);
+    }
+
+    [Fact]
+    public void GetIcon_WhenMultipleIconTypesExist_ShouldReturnCorrectIcons()
+    {
+        // Arrange
+        var iconFactory = new IconFactory();
+        var textIcon = iconFactory.GetIcon("Text File");
+        var imageIcon = iconFactory.GetIcon("Image File");
+
+        // Act
+        var retrievedTextIcon = iconFactory.GetIcon("Text File");
+        var retrievedImageIcon = iconFactory.GetIcon("Image File");
+
+        // Assert
+        retrievedTextIcon.Should().BeSameAs(textIcon);
+        retrievedImageIcon.Should().BeSameAs(imageIcon);
+    }
+}
